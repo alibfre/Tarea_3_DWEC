81458d0f28c431bcb0415812ac0b7ca664d5e398
alibfre <alibfre@gmail.com>
2026-05-14 13:55:13 +0200

    Creado y codificado el GameCanvas dentro de src/components

---
+++ src/components/GameCanvas.vue
@@ -0,0 +1,18 @@
+<template>
+  <canvas ref="canvas" :width="W" :height="H" />
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import { useGame } from '../composables/useGame.js';
+import { CANVAS_W, CANVAS_H } from '../constants/game.js';
+
+const W = CANVAS_W;
+const H = CANVAS_H;
+const canvas = ref(null);
+
+const { mount, unmount } = inject('game');
+
+onMounted(() => mount(canvas));
+onUnmounted(() => unmount());
+</script>
